b7764c1e3eda736a2c326d69435b12bcd1941358
tashi tsering <tatsering54@gmail.com>
2016-12-01 19:42:39 +0530

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,27 @@
+class Parent
+{
+	private void method1()
+	{
+		System.out.println ("Parent's method1()");
+	}
+	public void method2() 
+	{
+		System.out.println ("Parent's method2()");
+		method1();
+	}
+}
+	class Child extends Parent 
+	{
+	public void method1() 
+	{
+		System.out.println ("Child's method1()");
+	}
+ 	public static void main(String args[]) // Run - Time error Main class need to declare inside the class  
+	{
+		Parent p = new Child();
+		p.method2();
+ 	}
+}
+
+
+
